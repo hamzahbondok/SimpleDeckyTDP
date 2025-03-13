82ea22c6862f09d7dd93299e305e631c2e729c4e
hamzahbondok <hamzah.bondok@gmail.com>
2025-03-12 19:54:35 -0600

    aaaa

--- src/redux-modules/uiSlice.ts
+++ src/redux-modules/uiSlice.ts
@@ -1,12 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 // import type { PayloadAction } from "@reduxjs/toolkit";
-import { RootState } from "./store";
 
+type UiStateType = {
+  isDesktop?: boolean;
+};
 
 const initialState: UiStateType = {
-  isDesktop: false,
+  isDesktop: false
 };
 
-export const selectIsDesktop = (state: RootState) => {
-  return state.ui.isDesktop;
-};
+export const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    // setIsDesktop(state, action: PayloadAction<boolean>) {
+    //   state.isDesktop = action.payload;
+    // },
+  },
+});
